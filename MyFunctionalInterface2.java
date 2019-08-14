b2647140709dfc2a49b278754c1be1638b82bf83
vamsi1981 <vamsi.futurama@gmail.com>
2019-08-14 17:36:20 -0400

    MyFunctionalInterface2.java created
    MyFunctionalInterface2 File created for defining Functional Interfaces

---
+++ MyFunctionalInterface2.java
@@ -0,0 +1,11 @@
+public interface MyFunctionalInterface2{
+    public void execute();
+
+    public default void print(String text) {
+        System.out.println(text);
+    }
+
+    public static void print(String text, PrintWriter writer) throws IOException {
+        writer.write(text);
+    }
+}
