c1c42f565098c70fad5cf5e03ec70173b5af8d1e
vamsi1981 <vamsi.futurama@gmail.com>
2019-08-16 17:50:59 -0400

    Update LambdaExample.java

--- LambdaExample.java
+++ LambdaExample.java
@@ -1,3 +1,13 @@
+/*
+Lambda expressions basically express instances of functional interfaces (An interface with single abstract method is called functional interface. An example is java.lang.Runnable). lambda expressions implement the only abstract function and therefore implement functional interfaces
+
+lambda expressions are added in Java 8 and provide below functionalities.
+
+Enable to treat functionality as a method argument, or code as data.
+A function that can be created without belonging to any class.
+A lambda expression can be passed around as if it was an object and executed on demand.
+*/
+
 // Java program to demonstrate lambda expressions 
 // to implement a user defined functional interface. 
 
